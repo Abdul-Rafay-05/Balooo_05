8faafe7b3fbafbf34d65f8c7e25d3c64e79eb4aa
Abdul-Rafay-05 <fp5166605@gmail.com>
2026-04-17 21:23:35 +0500

    Update module.py

--- module.py
+++ module.py
@@ -1,5 +1,7 @@
 import pyjokes
-
+# in this code i leran about comments in programming
 print("printing jokesss...")
 joke = pyjokes.get_joke()
 print(joke)
+'''that my first program
+and that my firs comment '''
